75766c5bf564a26cf7741746e423e43719f4af50
McKinnon, Zachary W <Zachary.McKinnon@nov.com>
2015-12-16 12:48:12 -0600

    Set temporary mongodb connection and connect

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   database: {
-    url: ''
+    url: 'mongodb://future150-temp:chEPgdwNh9C4f3Lg@ds033135.mongolab.com:33135/heroku_ql792sq4'
   }
 };
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express'),
   port = process.env.PORT || 8080,
   config = require('./config');
 
-//mongoose.connect(config.database.url);
+mongoose.connect(config.database.url);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
