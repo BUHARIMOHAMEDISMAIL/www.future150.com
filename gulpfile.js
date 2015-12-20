1d2b50958bd5a68b0b30a07a127090c4194d0df5
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2015-12-20 09:01:02 -0600

    Add jshint to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp'),
-  jasmine = require('gulp-jasmine-node');
+  jasmine = require('gulp-jasmine-node'),
+  jshint = require('gulp-jshint'),
+  stylish = require('jshint-stylish');
 
-gulp.task('test', ['jasmine']);
+gulp.task('test', ['lint', 'jasmine']);
 
 gulp.task('jasmine', function () {
   return gulp.src('tests/**/*.spec.js')
     .pipe(jasmine());
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['./*.js', './app/**/*.js', './tests/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'));
 });
