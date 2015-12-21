ef7771f503ec340109978bb0bca84611226b29bf
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2015-12-20 22:58:08 -0600

    Add article controller test

---
+++ tests/controllers/articleController.spec.js
@@ -0,0 +1,7 @@
+var articleController = require('../../app/controllers/articleController');
+
+describe('Article Controller', function() {
+  it('should be defined', function() {
+    expect(articleController).toBeDefined();
+  });
+});
