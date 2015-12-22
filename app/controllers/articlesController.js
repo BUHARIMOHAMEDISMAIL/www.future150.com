097da30797e76082acc55c2fc0acb25f77c7399d
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2015-12-22 11:09:10 -0600

    Wire up articles controller to database

--- app/controllers/articlesController.js
+++ app/controllers/articlesController.js
@@ -1,41 +1,26 @@
 var express = require('express'),
-router = express.Router();
+  router = express.Router(),
+  Article = require('../models/article');
 
 router.get('/articles', function(req, res) {
-    var result =  {
-      count: 4,
-      articles: [
-        {
-          slug: 'looking-at-the-1-recruiting-class-kentucky-wildcats',
-          description: 'After picking up a commitment from elite SG Malik Monk, UK vaulted over Duke to take over the #1 spot.',
-          date: '',
-          title: 'Looking At The #1 Recruiting Class: Kentucky Wildcats',
-          imageUrl: '//future150.com/images/made/images/uploads/articles/wenyen-gabriel-kentucky-commit_643_341shar-60-.5-10_s_c1_c_c.png'
-        },
-        {
-          slug: 'johnny-newman-talks-recruitment-goals-for-this-season',
-          description: '2018 wing John Newman is becoming a big piece for Greensboro Day, and is gaining interest.',
-          date: '',
-          title: 'Johnny Newman Talks Recruitment And Goals',
-          imageUrl: '//future150.com/images/made/images/uploads/articles/Johnny_Newman_643_341_90shar-60-.5-10_s_c1_c_c.JPG'
-        },
-        {
-          slug: 'early-signing-period-comes-to-end-with-a-bang',
-          description: 'The early signing period came to a close in style on Wednesday, as a trio of top-40 recruits all announced their college destinations.',
-          date: '',
-          title: 'Early Signing Period Comes to End With A Bang',
-          imageUrl: '//future150.com/images/made/images/uploads/articles/Skal-Labissiere-Malik-Monk_643_341shar-60-.5-10_s_c1_c_t.png'
-        },
-        {
-          slug: 'tripp-greene-showing-promise',
-          description: '2018 G Tripp Greene has been showing promise as a potential lead guard at the D1 level',
-          date: '',
-          title: 'Tripp Greene Showing Promise',
-          imageUrl: '//future150.com/images/made/images/uploads/articles/IMG_1054_643_341_90shar-60-.5-10_s_c1_c_c.JPG'
+  Article.find()
+    .sort('title')
+    .skip((req.query.page - 1 || 0) * 2)
+    .limit(req.query.pageSize || 10)
+    .exec(function(err, articles) {
+      if (err) {
+        throw err;
+      }
+      Article.count().exec(function(err, count) {
+        if (err) {
+          throw err;
         }
-      ]
-    };
-    res.json(result);
-  });
+        res.json({
+          count: count,
+          articles: articles
+        });
+      });
+    });
+});
 
 module.exports = router;
