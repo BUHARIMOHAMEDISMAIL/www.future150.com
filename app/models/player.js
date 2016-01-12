355745b2804b8d82e93d70d67cb91ca66993e88d
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2016-01-11 20:41:33 -0600

    Add fields to player model

--- app/models/player.js
+++ app/models/player.js
@@ -4,11 +4,21 @@ module.exports = mongoose.model('Player', {
   legacyId: Number,
   slug: String,
   description: String,
+  createdDate: Date,
+  lastUpdatedDate: Date,
   firstName: String,
   position: String,
   lastName: String,
   imageUrl: String,
   hometownCity: String,
   hometownState: String,
-  class: Number
+  class: Number,
+  aauTeam: String,
+  heightFeet: Number,
+  heightInches: Number,
+  highSchool: String,
+  starts: Number,
+  weight: Number,
+  notes: String,
+  projections: String
 });
