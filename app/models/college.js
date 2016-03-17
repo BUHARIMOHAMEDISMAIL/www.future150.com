73bbc162b1e6a383e485e8e0ed2ffe177a261b3b
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2016-03-16 21:40:29 -0500

    Add slug to college

--- app/models/college.js
+++ app/models/college.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('College', {
   legacyId: Number,
+  slug: String,
   name: String,
   initials: String,
   logoImageUrl: String
