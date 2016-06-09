61e29ec76e112e83e549214a9251d956d9e76ca6
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2016-06-08 19:26:22 -0500

    Remove type from events

--- app/models/camp.js
+++ app/models/camp.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Camp', {
   slug: String,
-  type: String,
   title: String,
   city: String,
   state: String,
--- app/models/tournament.js
+++ app/models/tournament.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Tournament', {
   slug: String,
-  type: String,
   title: String,
   city: String,
   state: String,
