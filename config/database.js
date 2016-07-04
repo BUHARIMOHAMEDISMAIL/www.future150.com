53a52fd7c98b368c07a46e3f14d6b3979573949c
Mohamed Ismail <ismailbuharimohamed@gmail.com>
2016-07-04 12:57:37 +0530

    Update database.js
    Please ignore the second line.
    For test locally i just insert this line

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: process.env.DATABASE_URL || 'mongodb://future150-temp:chEPgdwNh9C4f3Lg@ds033135.mongolab.com:33135/heroku_ql792sq4'
-  // url: process.env.DATABASE_URL || 'mongodb://localhost/nwfut'
+  url: process.env.DATABASE_URL || 'mongodb://future150-temp:chEPgdwNh9C4f3Lg@ds033135.mongolab.com:33135/heroku_ql792sq4',
+  url: process.env.DATABASE_URL || 'mongodb://localhost/nwfut'
 };
