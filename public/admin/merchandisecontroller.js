6f42ea2ba531b4cc38b6cf6742c3135d0df25556
Mohamed Ismail <ismailbuharimohamed@gmail.com>
2016-07-04 12:52:07 +0530

    Update merchandisecontroller.js

--- public/admin/merchandisecontroller.js
+++ public/admin/merchandisecontroller.js
@@ -15,19 +15,7 @@
       merchandiseService.getAll().then(function(result) {
         $scope.contents = result.merchandises;
         console.log(result.merchandises);
-        // console.log(result.dummies._id);
-        // vm.count = result.count;
-        // vm.start = (vm.page - 1) * vm.pageSize + 1;
-        // vm.end = (vm.start + vm.pageSize < result.count) ? (vm.page - 1) * vm.pageSize + vm.pageSize : result.count;
       });
     }
-
-
-    // $scope.save = function(dummy) {
-    //   console.log("inside save dummy function");
-    //   dummiesService.save(dummy).then(function() {
-    //     $state.go('dashboard');
-    //   });
-    // }
   }
 })();
